11202a38a9164120ed9747d478a4d4038673180a
Jeff <jeffersonorozco24@gmail.com>
2026-02-23 22:00:44 -0500

    FEAT: add texts and examples

---
+++ fundamentals/texts.py
@@ -0,0 +1,7 @@
+texto = "holaaaaa"
+
+print(texto.upper()) #Convierte a mayuscula
+print(texto.lower()) #Convierte a minusculas
+print(texto.title()) #Primera letra en mayuscula
+print(texto.strip()) #Elimina espacios extras
+print(texto.replace("a","o")) #reemplaza letras
