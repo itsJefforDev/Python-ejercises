c905848187544d266babf61f4299a39f53cd4c7e
Jeff <jeffersonorozco24@gmail.com>
2026-02-24 22:55:20 -0500

    FEAT: add example to switch

--- fundamentals/switch.py
+++ fundamentals/switch.py
@@ -1,3 +1,19 @@
-value = input("")
+dia = 1
 
-match()
+match(dia):
+    case 1:
+        print("lunes")
+    case 2:
+        print("martes")
+    case 3:
+        print("miercoles")
+    case 4:
+        print("jueves")
+    case 5:
+        print("viernes")
+    case 6:
+        print("sabado")
+    case 7:
+        print("domingo")
+    case _:
+        print("fuera de rango")
