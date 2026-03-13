e01f1572bede5585e1b3356ab93379a0b3746834
Jeff <jeffersonorozco24@gmail.com>
2026-03-12 22:33:14 -0500

    FEAT: add doWhile example

---
+++ fundamentals/doWhileLoop.py
@@ -0,0 +1,13 @@
+
+#En Python no existe una estructura do...while nativa como en C, Java o JavaScript.
+#Pero se puede simular usando un while True y un break.
+
+numero = 0
+
+while True:
+    numero = int(input("Ingresa un número mayor que 10: "))
+    
+    if numero > 10:
+        break
+
+print("Número válido:", numero)
