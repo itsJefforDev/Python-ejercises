5e1fbdfe573c588695f66c989165adbf3122ccff
Jeff <jeffersonorozco24@gmail.com>
2026-03-15 23:42:05 -0500

    FEAT: add comment to 1

--- taller ciclos/1.py
+++ taller ciclos/1.py
@@ -1,3 +1,9 @@
+# Ejercicio 1 – Contador con while
+# Crear un programa que imprima los números del 1 al 10 usando while.
+# Resultado esperado:
+# 1 2 3 4 5 6 7 8 9 10
+
+
 contador = 1
 
 while contador <= 10:
