fe69b42d6ad246bdf1fe6b213f252b346ef92860
Jeff <jeffersonorozco24@gmail.com>
2026-03-19 23:09:28 -0500

    docs: add comment to 4

--- taller ciclos/4.py
+++ taller ciclos/4.py
@@ -1,3 +1,9 @@
+# Ejercicio 4 – Detener el bucle (break)
+# Crear un programa que imprima los números del 1 al 10, pero debe 
+# detenerse cuando llegue al número 7.
+# Debe usar break.
+
+
 count = 0
 
 while (count<=10):
