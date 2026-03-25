d709a83f79757fb839bdec7420781ed75fe3180b
Jeff <jeffersonorozco24@gmail.com>
2026-03-24 23:05:29 -0500

    docs: add comment to 8

--- fundamentals/taller iterables/8.py
+++ fundamentals/taller iterables/8.py
@@ -1,3 +1,12 @@
+#     8. zip con palabra  
+# Pídele al usuario una palabra. 
+# Crea una lista de números desde 1 hasta la longitud de la palabra. 
+# Luego usa zip() para emparejar cada número con una letra. 
+# Usa zip() para mostrar: 
+# 1 - p 
+# 2 - y 
+# 3 – t 
+
 text = input("Ingresa una palabra: ")
 
 numeros = list(range(1, len(text) + 1))
