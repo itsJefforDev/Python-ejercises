0a37778c20bf2f223dfbf38c6fd28372056b6abd
Jeff <jeffersonorozco24@gmail.com>
2026-04-07 23:53:24 -0500

    FIX: error in 12

--- fundamentals/taller iterables/12.py
+++ fundamentals/taller iterables/12.py
@@ -4,11 +4,4 @@ ana = {"Python", "Diseño UX", "Estadística", "Cálculo"}
 finalList = zip(juan, ana)
 
 
-for juan, ana in finalList:
-
-    #print("-----Materia en comun-------")
-    if(juan == ana):
-        print(juan)
-
-    if(juan=)
 
